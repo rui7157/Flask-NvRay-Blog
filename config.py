2ae32141d97842ad00929eedf7810e700acef979
NvRay <goingrui@gmail.com>
2016-02-01 21:06:09 +0800

    sae add mysql++++

--- config.py
+++ config.py
@@ -12,13 +12,18 @@ class Config:
         print "running Config class"
 class SinaappSae(Config):
     import MySQLdb
+    """
+    用户名：SAE_MYSQL_USER ， 密码：SAE_MYSQL_PASS
+
+主库域名：SAE_MYSQL_HOST_M ，端口：SAE_MYSQL_PORT
+    """
     SECRET_KEY = os.environ.get("SECRET_KEY")
     MYSQL_HOST=os.environ.get('MYSQL_HOST')
     MYSQL_USER=os.environ.get('ACCESSKEY')
     MYSQL_PASSWORD=os.environ.get('SECRETKEY')
     MYSQL_PORT=os.environ.get('MYSQL_PORT')
     MYSQL_DB = 'app_' + os.environ.get('ACCESS_KEY')
-    SQLALCHEMY_DATABASE_URI="mysql+MySQLdb://%s:%s@%s:%s/%s" %(os.environ.get('ACCESSKEY'),os.environ.get('SECRETKEY'),os.environ.get('MYSQL_HOST'),int(os.environ.get('SAE_MYSQL_PORT')),'app_' + os.environ.get('APPNAME'))
+    SQLALCHEMY_DATABASE_URI="mysql+MySQLdb://%s:%s@%s:%s/%s" %(os.environ.get('SAE_MYSQL_USER'),os.environ.get('SAE_MYSQL_PASS'),os.environ.get('SAE_MYSQL_HOST_M'),int(os.environ.get('SAE_MYSQL_PORT')),'app_' + os.environ.get('ACCESS_KEY'))
 
 
 
